469f8a35ce2d2b7e4c5c38a83ae4cabec98babc8
ellenbechtel <ellen.bechtel@gmail.com>
2020-01-21 19:43:17 -0500

    combined the API fetches, tried a new circle attempt

--- homework/01-16/script.js
+++ homework/01-16/script.js
@@ -18,7 +18,7 @@
                 .attr("width",width)
                 .attr("height",height);
 
-            var realTimeURL = "https://whiteboard.datawheel.us/api/google-analytics/realtime/51808252"
+            var realTimeURL = "https://whiteboard.datawheel.us/api/google-analytics/realtime/random"
             
             var interval = 10 * 1000; // 10 seconds
        
@@ -38,64 +38,47 @@
                 .attr("x",30)
                 .attr("y",30)
                 .attr("stroke","black")
-                .text("This is a test circle");
+                .text("This is a test blob");
 
 
 
-            
-            //////////////////////////////////
-            // INITIALIZE THE VIZ WITH THE FIRST CALL OF THE API
-            //////////////////////////////////
 
-            var users = [];
-
-            function fetchData() {
-                d3.json(realTimeURL, function(error,users) {  // Is this the first place where "users" gets declared a variable?
-
-                    console.log("users:", users);
-
-                    d3.select("#users").html(users);
-
-                    var circle = svg.selectAll("circle")
-                        .data(users)
-                        .enter()
-                        .append("circle")
-                            .attr("cx", 100)
-                            .attr("cy", 100)
-                            .attr("r", users*10)
-                            .attr("fill","teal");
-                   
-                });
-            }
 
             //////////////////////////////////
             // ENTER UPDATE EXIT, BITCHES!!!!!
             //////////////////////////////////
 
-            function updateData() {
+            var users = [];
+                console.log("users1:", users);
+
+            function fetchData() {
+
                 d3.json(realTimeURL, function(error,users) {
+
+                    console.log("users2:", users);
  
-                    var nextCirc = svg.selectAll("circ")
-                        .data(users);
+                    var circle = svg.selectAll("circle")
+                        .data(users)
+                        .enter()
+                        .append("circle")
+                            .attr("cx", width/2)
+                            .attr("cy", height/2)
+                            .attr("r", 0)
+                            .attr("fill","teal")
+                        .merge(circle)
+                            .transition()
+                            .attr("cx", width/2)
+                            .attr("cy", height/2)
+                            .attr("r", users*3)
+                            .attr("fill","teal");
                     
-                    nextCirc.enter().append("circ")
-                        .attr("cx", width/2)
-                        .attr("cy", height/2)
-                        .attr("r", 0)
-                        .attr("fill","teal")
-                    .merge(nextCirc)
-                        .transition()
-                        .attr("cx", width/2)
-                        .attr("cy", height/2)
-                        .attr("r", users*15)
-                        .attr("fill","teal");
-                    
-                    nextCirc.exit()
+                    circle.exit()
                         .transition()
                         .attr("r", 0)
                         .remove();
                         
                 });
+
             }
 
             //////////////////////////////////
@@ -104,8 +87,8 @@
 
         
             fetchData(); // initialize the data
-            updateData();
-            setInterval(updateData, interval);
+            setInterval(fetchData, interval);
         
-           
-    
+
+
+
