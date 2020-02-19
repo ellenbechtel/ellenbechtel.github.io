cd7a989a6c6c8e9bc534644f855c13065177c10b
ellenbechtel <ellen.bechtel@gmail.com>
2020-02-19 08:27:58 -0500

    making color scale

--- projects/eggs/script.js
+++ projects/eggs/script.js
@@ -37,9 +37,6 @@ window.addEventListener('scroll', function () {
 // Get values of the toggle switches // Tutorial at https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_radio_checked2
 
 var isChecked = document.getElementById("radio-appearance").checked;
-  console.log(isChecked);
-
-
 
 
 
@@ -48,11 +45,11 @@ var isChecked = document.getElementById("radio-appearance").checked;
 // SET STATIC VARIABLES
 //////////////////////////////////
 var margin = {top: 30, right: 30, bottom: 30, left: 30};
-var width = 120;
-var height = 120;
+var width = 230;
+var height = 230;
 
-var chartWidth = width - margin.left - margin.right;
-var chartHeight = height - margin.top - margin.bottom;
+var chartWidth = width;
+var chartHeight = height;
 
 
 /////////////////////////////////
@@ -70,10 +67,8 @@ Promise.all(promises).then(function(data) {
   var products = data[0];
   var eggsplainer = data[1];
 
+
   console.log(products);
-  console.log(eggsplainer);
-
-
   /////////////////////////////////
   // MANIPULATE THE DATA TO GET SOME THINGS WE WANT
   //////////////////////////////////
@@ -102,23 +97,24 @@ Promise.all(promises).then(function(data) {
   };
 
   // Keyword Index Scale
-  var keywordScale = d3.scaleSqrt()
+  var keywordScale = d3.scaleOrdinal()
     .domain([]) // how do I dynamically calculate the length of the keyword array?  .length something?
     .range([]); // how do I get this to be scaled to the size of the svg?
 
- 
-
-  // Make new array of products with values that are replaced by the keywork lookups in eggsplainer
-
-
-
-    
+   
   // Make the Bad-Neutral-Good-Misleading Color Scale
 
   var colorScale = d3.scaleOrdinal()
-    .domain(["Bad", "Neutral", "Good","Misleading"]) // change this to "too good to be true"
-    .range(["#4c6375", "#b1d4ce", "#fed200", "#fa8c00"]);
+    .domain(["Bad", "Neutral", "Good", "Misleading", "none"]) // change this to "too good to be true"
+    .range(["#4c6375", "#b1d4ce", "#fed200", "#fa8c00", "none"]);
 
+
+  // Make the Radius Scale, to say what order the circles should go in
+
+  var radRatio = 4;
+  var radiusScale = d3.scaleOrdinal() 
+    .domain(["Size", "Color", "Grade", "Cholesterol", "Fat Description", "Vitamins", "Omega 3 ", "Feed", "Pesticides", "GMO in Feed", "Non-GMO Certified", "USDA Organic", "Range", "Fertile", "Certified Humane", "Antibiotics", "Hormones", "Kosher", "Container Material", "Pictures ", "Other Certifications", "Says 'Fresh' Anywhere", "Tag Lines"])
+    .range([1*radRatio, 2*radRatio, 3*radRatio, 4*radRatio, 5*radRatio, 6*radRatio, 7*radRatio, 8*radRatio, 9*radRatio, 10*radRatio, 11*radRatio, 12*radRatio, 13*radRatio, 14*radRatio, 15*radRatio, 16*radRatio, 17*radRatio, 18*radRatio, 19*radRatio, 20*radRatio, 21*radRatio, 22*radRatio, 23*radRatio]);
 
 
   // Make the Regulated-NonRegulated Dash Scale
@@ -126,7 +122,6 @@ Promise.all(promises).then(function(data) {
   var regulationScale = d3.scaleOrdinal()  // use .dasharray in d3 attr
     .domain(["Regulated", "Optional", "Unregulated"])
     .range(["0", "4 1", "1 4"]);
-
 
 
   /////////////////////////////////
@@ -147,63 +142,53 @@ Promise.all(promises).then(function(data) {
           });
 
 
-
-
-
-
   // Draw Circles in each SVG
-
-
-  d3.selectAll(".miniEgg").each(function(d) {
+  d3.selectAll(".miniEgg").each(function(d) { // for each SVG, which is bound to the data "products"
 
      // Make deconstructed data
+    var deconstructed = []; 
 
-    var deconstructed = [];
-
-    for(var property in d) {
-      var val = d[property];
-
-      var o = {keyword: property, value: val} 
-
-      deconstructed.push(o);
+    for(var property in d) { // for every column header for every object in "products"...
+      var val = d[property]; // this is kinda the same as d.property, but is notated differently. also we're making a thing called property here? 
+      var o = {keyword: property, value: val} // making an object pair and saving it in var o
+      deconstructed.push(o); // push the object with the pairs into the empty var deconstructed
     };
+    //console.log(deconstructed); // deconstructed should be an object like {keyword: "Size"; value: "Large"} for each svg
 
-      console.log(deconstructed);
+    var this_svg = d3.select(this); // select the SVG we're working on at this moment in the loop
 
-
-
-    var this_svg = d3.select(this);
-    console.log(this_svg);
     this_svg.selectAll("circle")
-      .data(deconstructed) //deconstructed data
+      .data(deconstructed) //bind the deconstructed data
       .enter()
       .append("circle")
         .attr("cx", chartWidth/2)
         .attr("cy", chartHeight/2)
         .attr("fill", "none")
-        .attr("stroke", function(e) {
-
-          var meaning = lookup(e.value);
-
-          // Make this a lookup function to call here and at the mouseover
-
-          console.log(e);
+        .attr("stroke", function(e) { // eventually Make this a lookup function to call here and at the mouseover
+          
+          //var meaning = lookup(e.value);
           
           var filtered = eggsplainer.filter(function(f) {
-            return f.phrase === e.value;
-          });
-          if(filtered.length == 1) {
-                    
-            //return colorScale(filtered[0].meaning);
+            
 
-          } else {
-            return "none";
-          }
- 
-        })
+              return f.phrase === e.value; // for the deconstructed data, which only has one value (like "Large"), go through eggsplainer (f.phrase) and see if there's anything that matches that value.  There should ony be one thing. 
+              console.log(e.value);
+            });
+
+            if(filtered.length == 1) { // this is a filter to say "if there's ANYTHING in the array called filtered, do something with it".  There should only be one thing, or none. Not more.
+                      
+              return colorScale(filtered[0].meaning); // Whatever the meaning is, color code it
+
+            } else {
+
+              return "none"; // if there was no value, then there's no corresponding meaning, so don't color code it.
+            
+            };
+         })
         .attr("stroke-width", 1)
-        .attr("r", function(e,i) {
-          return i*3;
+        // add a stroke-dash array lookup here too
+        .attr("r", function(d) {
+          return radiusScale(d.keyword);
           //return radiusScale(d.keyword)
         });
 
@@ -232,7 +217,7 @@ Promise.all(promises).then(function(data) {
           .attr("y", chartHeight)
           .attr("x", chartWidth/2)
           .attr("fill", "black") // code this with a colorscale
-          .text(function(d){ return(d.Brand)});
+          .text(function(d){ return(d.name)});
   svg
       .append("text")
           .attr("class", "store")
@@ -240,7 +225,7 @@ Promise.all(promises).then(function(data) {
           .attr("y", chartHeight)
           .attr("x", chartWidth/2)
           .attr("fill", "black") // code this with a colorscale
-          .text(function(d){ return(d.Store)});
+          .text(function(d){ return(d.store)});
 
     
   /////////////////////////////////
@@ -273,10 +258,16 @@ Promise.all(promises).then(function(data) {
 
 
 function parseProducts(data) {
+
+
+///////// FILTER DOWN THE PRODUCTS PARSE FUNCTION SO IT'S ONLY THE KEYWORDS WE WANT
+
+
     var d = {};
     d.img = data.IMG;
     d.store = data.Store;
     d.brand = data.Brand;
+    d.name = data.Name;
     d.ct = data.Count;
     d.price = data.Price;
     d.pricePerEgg = data.perEgg;
@@ -285,7 +276,6 @@ function parseProducts(data) {
     d.color = data.Color;
     d.grade = data.Grade;
     d.range = data.Range;
-    d.rangeQ = data.RangeQuote;
     d.humane = data.Humane;
     d.org = data.USDAOrganic;
     d.gmo = data.NonGMOCertified;
@@ -298,7 +288,6 @@ function parseProducts(data) {
     d.vit = data.Vitamins;
     d.fat = data.Fat;
     d.feed = data.Feed;
-    d.feedQ = data.FeedQuote;
     d.kosh = data.Kosher;
     d.gmoFeed = data.GMOinFeed;
     d.pest = data.Pesticides;
@@ -326,6 +315,5 @@ function parseEggsplainer(data) {
   return d;
 
 };
-
 
 
