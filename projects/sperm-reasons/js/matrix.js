16d2a7e9f1c7b4bdeb08217055070094c91a639b
ellenbechtel <ellen.bechtel@gmail.com>
2020-04-10 17:18:04 -0400

    first attempt at electrophoresis diagram

--- projects/sperm-reasons/js/matrix.js
+++ projects/sperm-reasons/js/matrix.js
@@ -1,5 +1,5 @@
 /////////////////////////////////
-// BEEWARM CODE!
+// MATRIX CODE!
 //////////////////////////////////
 
 
@@ -9,8 +9,80 @@
 
 console.clear()
 
-var width = document.querySelector("#beeswarm").clientWidth;
-var height = document.querySelector("#beeswarm").clientHeight;
+var width = document.querySelector("#matrix").clientWidth;
+var height = document.querySelector("#matrix").clientHeight;
+var margin = {
+    top: 20,
+    right: 20,
+    bottom: 100,
+    left: 20 
+};
+var chartWidth = width-margin.left-margin.right;
+var chartHeight = height - margin.top - margin.bottom;
+
+var svg = d3.select("#matrix")
+    .attr("width", chartWidth)
+    .attr("height", chartHeight);
+
+// Make an object of the reasons why a donor might donate and its altruism value
+var reasons = [
+    {code: "0", value: 0, label: "Genetic Pride", because: "he is proud of his genetics"},
+    {code: "1", value: 1, label: "Money", because: "of the money"},
+    {code: "2", value: 2, label: "Why Not", because: "why the heck not"},
+    {code: "3", value: 3, label: "Curiosity", because: "he is curious"},
+    {code: "4", value: 4, label: "Likes Donating", because: "he likes donating in general"},
+    {code: "5", value: 5, label: "Don't Have, Like, or Want Kids", because: "he doesn't have, like, or want kids"},
+    {code: "6", value: 6, label: "Has, Likes, or Wants Kids", because: "he has, likes, or wants kids"},
+    {code: "7", value: 7, label: "Knows Someone", because: "he knows someone"},
+    {code: "8", value: 8, label: "It's a Good Deed", because: "it's a good deed"},
+    {code: "9", value: 9, label: "Wants to Help Others", because: "he wants to help others"}
+   ];
+
+// Make Scales
+var orders = ["1","2","3","4"];
+var opacities = ["1","0.5","0.2","0.1"]
+var opacityScale = d3.scaleOrdinal()
+    .domain(orders)
+    .range(opacities);
+
+var xScale = d3.scaleBand()
+    .domain(reasons.map(function (d) { return d["label"]; }))
+    .rangeRound([0, chartWidth]);
+
+var yScale = d3.scaleBand()
+    .rangeRound([chartHeight, 0]); // set the domain afterwards, once all the donors are loaded
+
+
+/////////////////////////////////
+// LOAD THE DATA!
+//////////////////////////////////
+
+
+d3.csv("donorReasons.csv", function (error, donorsR) {
+
+    // sort the donors by their altruisticness code, most altruistic to most selfish
+    donorsR = donorsR.sort(function (a,b) {return d3.ascending(b.code, a.code);});
+        console.log(donorsR);
+
+    // get an array of all the donor names in the database, set that to the domain of Y scale
+    var names = donorsR.map(function (d) { return d["name"]; });
+    yScale.domain(names);
+
+    opacityScale.domain();
+
+
+    // begin making some groupings
+
+    var rows = svg.select("#donorRows").selectAll(".row")
+        .data(donorsR)
+        .enter()
+        .append("g")
+            .attr("class","row")
+            .attr("transform", function(d) { return "translate(0," + yScale(d["name"]) +")"; }); // this part is not working
+
+});
+/*
+
 
 var transitionTime = 1 * 1000; // 1 second
 var radius = 6;
@@ -22,6 +94,10 @@ var gravityStrength = -4;
 var friction = 0.5;
 var yGravity = 0.3;
 var collPadding = 3;
+
+
+
+
 
 var margin = {
     top: 20,
@@ -360,3 +436,8 @@ d3.csv("./donors.csv", function(donors) {
 
 
 });
+
+
+
+
+*/
