4e9133cc8dbd56420d8ad3f32b8ac4b00e3748e4
ellenbechtel <ellen.bechtel@gmail.com>
2021-04-14 16:37:12 -0400

    got a bunch of updates! new data structure

--- projects/visual-cognition-dendrogram/scripts/dendro.js
+++ projects/visual-cognition-dendrogram/scripts/dendro.js
@@ -1,7 +1,7 @@
 
 // set the dimensions and margins of the graph
-var width = 460
-var height = 460
+var width = 700
+var height = 700
 var radius = width / 2 // radius of the dendrogram
 var json = "https://spreadsheets.google.com/feeds/cells/1nIicYXU8mvfRoOoD1MbJCaGa6p_r95eAHgW2DDMIH-Y/1/public/full?alt=json"
 var test = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_dendrogram.json"
@@ -13,340 +13,42 @@ var svg = d3.select("#dendrogram")
   .append("svg")
     .attr("width", width)
     .attr("height", height)
+    .attr("id","#dendrogram-svg")
   .append("g")
     .attr("transform", "translate(" + radius + "," + radius + ")");
 
 // Node Style
 var rad = 5;
-var fill = "#69b3a2";
-var stroke = "black";
-var strokeWidth = .5;
 
 // Link Style
 
 
 // This is the data from the Google Sheet
-var vcData = [
-    {
-      "Tier_1": "Visual Channel",
-      "Tier_2": "V1",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Form"
-    },
-    {
-      "Tier_1": "Visual Channel",
-      "Tier_2": "V1",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Color"
-    },
-    {
-      "Tier_1": "Visual Channel",
-      "Tier_2": "V1",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Movement1"
-    },
-    {
-      "Tier_1": "Visual Channel",
-      "Tier_2": "Visual Hierarchy",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Color1"
-    },
-    {
-      "Tier_1": "Visual Channel",
-      "Tier_2": "Visual Hierarchy",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Orientation"
-    },
-    {
-      "Tier_1": "Visual Channel",
-      "Tier_2": "Visual Hierarchy",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Size"
-    },
-    {
-      "Tier_1": "Visual Channel",
-      "Tier_2": "Visual Hierarchy",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Motion"
-    },
-    {
-      "Tier_1": "Visual Channel",
-      "Tier_2": "Visual Hierarchy",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Depth"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Qualitative Data",
-      "Tier_3": "Nominal data",
-      "Image URL": "",
-      "VC_Name": "Gender"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Qualitative Data",
-      "Tier_3": "Nominal data",
-      "Image URL": "",
-      "VC_Name": "Hair color"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Qualitative Data",
-      "Tier_3": "Nominal data",
-      "Image URL": "",
-      "VC_Name": "Ethnicity"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Qualitative Data",
-      "Tier_3": "Ordinal data",
-      "Image URL": "",
-      "VC_Name": "First, Second"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Qualitative Data",
-      "Tier_3": "Ordinal data",
-      "Image URL": "",
-      "VC_Name": "Letter grade"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Qualitative Data",
-      "Tier_3": "Ordinal data",
-      "Image URL": "",
-      "VC_Name": "Economic status"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Quantitative Data",
-      "Tier_3": "Discrete data",
-      "Image URL": "",
-      "VC_Name": "Students number"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Quantitative Data",
-      "Tier_3": "Discrete data",
-      "Image URL": "",
-      "VC_Name": "Workers number"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Quantitative Data",
-      "Tier_3": "Discrete data",
-      "Image URL": "",
-      "VC_Name": "Population"
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Quantitative Data",
-      "Tier_3": "Continuous data",
-      "Image URL": "",
-      "VC_Name": "Height of children "
-    },
-    {
-      "Tier_1": "Data Type",
-      "Tier_2": "Quantitative Data",
-      "Tier_3": "Continuous data",
-      "Image URL": "",
-      "VC_Name": "Speed of cars"
-    },
-    {
-      "Tier_1": "Color",
-      "Tier_2": "Color Dimensions",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Hue"
-    },
-    {
-      "Tier_1": "Color",
-      "Tier_2": "Color Dimensions",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Satuation"
-    },
-    {
-      "Tier_1": "Color",
-      "Tier_2": "Color Dimensions",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Brightness"
-    },
-    {
-      "Tier_1": "Color",
-      "Tier_2": "Color Discriminability",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Color perceptions differ"
-    },
-    {
-      "Tier_1": "Color",
-      "Tier_2": "Color Discriminability",
-      "Tier_3": "Mark types",
-      "Image URL": "",
-      "VC_Name": "Circles"
-    },
-    {
-      "Tier_1": "Color",
-      "Tier_2": "Color Discriminability",
-      "Tier_3": "Mark types",
-      "Image URL": "",
-      "VC_Name": "Rectangular"
-    },
-    {
-      "Tier_1": "Color",
-      "Tier_2": "Color Discriminability",
-      "Tier_3": "Mark types",
-      "Image URL": "",
-      "VC_Name": "Lines"
-    },
-    {
-      "Tier_1": "Color",
-      "Tier_2": "Color Discriminability",
-      "Tier_3": "Robust",
-      "Image URL": "",
-      "VC_Name": "Identifiable"
-    },
-    {
-      "Tier_1": "Attention",
-      "Tier_2": "",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Fomo"
-    },
-    {
-      "Tier_1": "Attention",
-      "Tier_2": "Disturbing",
-      "Tier_3": "Transition point",
-      "Image URL": "",
-      "VC_Name": "Symbol"
-    },
-    {
-      "Tier_1": "Attention",
-      "Tier_2": "Disturbing",
-      "Tier_3": "Transition point",
-      "Image URL": "",
-      "VC_Name": "Color2"
-    },
-    {
-      "Tier_1": "Attention",
-      "Tier_2": "Physiological network",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Alerting"
-    },
-    {
-      "Tier_1": "Attention",
-      "Tier_2": "Inattention blindness",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Orienting"
-    },
-    {
-      "Tier_1": "Attention",
-      "Tier_2": "Inattention blindness",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Executing"
-    },
-    {
-      "Tier_1": "Attention",
-      "Tier_2": "Inattention blindness",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "You see what you seek"
-    },
-    {
-      "Tier_1": "Motion",
-      "Tier_2": "Internal",
-      "Tier_3": "Eye movement",
-      "Image URL": "",
-      "VC_Name": "Capture objects"
-    },
-    {
-      "Tier_1": "Motion",
-      "Tier_2": "Internal",
-      "Tier_3": "Gesture",
-      "Image URL": "",
-      "VC_Name": "Understand contents"
-    },
-    {
-      "Tier_1": "Motion",
-      "Tier_2": "Internal",
-      "Tier_3": "Mind",
-      "Image URL": "",
-      "VC_Name": "Decode informtion"
-    },
-    {
-      "Tier_1": "Motion",
-      "Tier_2": "Extenal",
-      "Tier_3": "",
-      "Image URL": "",
-      "VC_Name": "Movement2"
-    },
-    {
-      "Tier_1": "Motion",
-      "Tier_2": "Extenal",
-      "Tier_3": "Variable changing",
-      "Image URL": "",
-      "VC_Name": "Color3"
-    },
-    {
-      "Tier_1": "Motion",
-      "Tier_2": "Extenal",
-      "Tier_3": "Variable changing",
-      "Image URL": "",
-      "VC_Name": "Brightness"
-    },
-    {
-      "Tier_1": "Motion",
-      "Tier_2": "Extenal",
-      "Tier_3": "Variable changing",
-      "Image URL": "",
-      "VC_Name": "Shapw"
-    },
-    {
-      "Tier_1": "Motion",
-      "Tier_2": "Extenal",
-      "Tier_3": "Variable changing",
-      "Image URL": "",
-      "VC_Name": "Size"
-    },
-    {
-      "Tier_1": "Motion",
-      "Tier_2": "Extenal",
-      "Tier_3": "Spatial",
-      "Image URL": "",
-      "VC_Name": "Enhance conceptual inference"
-    }
-   ];
-
+var vcData = [];
 console.log(vcData, "vc")
 
-// Now let's nest it properly
-var nest = d3.nest()
-    .key(function(d){
-        return d.Tier_1;
-    })
-    .key(function(d){
-        return d.Tier_2;
-    })
-    .key(function(d){
-        return d.Tier_3;
-    })
-   .entries(vcData);
+// // Now let's nest it properly
+// var nest = d3.nest()
+//     .key(function(d){
+//         return d.Tier_1;
+//     })
+//     .key(function(d){
+//         return d.Tier_2;
+//     })
+//     .key(function(d){
+//         return d.Tier_3;
+//     })
+//    .entries(vcData);
 
-var finalData = {key: "Visual Cognition", values: nest};
+// var finalData = {key: "Visual Cognition", values: nest};
+var root = d3.stratify()
+ .id(function(d) { 
+  console.log(d,"inside") 
+  return d.id; })
+ .parentId( function(d) { return d.parentId; })
+ (vcData);
 
+var finalData = root;
 console.log(finalData,"final")
 
 
@@ -372,19 +74,20 @@ svg.selectAll('path')
 .enter()
 .append('path')
   .attr("d", linksGenerator)
-  .style("fill", 'none')
-  .attr("stroke", '#ccc')
+  .attr("class","link");
 
 
 // Tooltip
 var tooltip = d3.select("#dendrogram")
   .append("div")
-  .style("opacity",1)
-  .attr("class", "tooltip")
+    .style("opacity",1)
+    .attr("class", "tooltip")
+
 
 // Extra Content
 var details = d3.select("#details")
-  .style("opacity",1)
+  .style("opacity",1);
+var topic = d3.select("#topic");
 
 // hover
 var mouseover = function(d){
@@ -398,9 +101,10 @@ var mouseover = function(d){
 var mousemove = function(d) {
 
   tooltip
-    .html(d.data.key)
-    .style("left", (d3.mouse(this)[0]+70) + "px")
-    .style("top", (d3.mouse(this)[1]) + "px")
+    .text(d.data.key)
+      .style("left", (d3.event.pageX + 5) + "px")
+      .style("top", (d3.event.pageY + 20) + "px");
+
 }
 
 // mouse leave
@@ -408,14 +112,15 @@ var mouseleave = function(d) {
   tooltip
     .style("opacity", 0)
   d3.select(this)
-    .style("stroke", "none")
-    .style("opacity", 0.8)
+    .attr("class", "visited")
+  
 }
 
 // click
 var click = function(d){
   console.log(d, "d")
-  details.html(d.data.key);
+  // details.html(d.data.key);
+  topic.text(d.data.key);
 }
 
 
@@ -429,10 +134,8 @@ svg.selectAll("g")
       return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
   })
   .append("circle")
+    .attr("class","node")
     .attr("r", rad)
-    .style("fill", fill)
-    .attr("stroke", stroke)
-    .style("stroke-width", strokeWidth)
   .on("mouseover", mouseover)
   .on("mousemove", mousemove)
   .on("mouseleave", mouseleave)
---
+++ projects/visual-cognition-dendrogram/scripts/radialdendro.js
@@ -0,0 +1,305 @@
+var width = d3.min([window.innerWidth, 700]),
+height = d3.min([window.innerWidth, 700]),
+radius = width/2,
+maxRad = 10,
+distance = 70, // radial spread
+duration = 100,
+depth = 5;
+
+// colors for nodes
+var colors = {
+    collapsed: "#666666",
+    expanded: "#cccccc",
+    leaf: "#000000",
+    highlight: "#d9664a"
+}
+
+
+var nodes,links;
+var i = 0;
+
+
+// Extra Content
+var tooltip = d3.select("#dendrogram")
+  .append("div")
+    .style("opacity",0)
+    .attr("class", "tooltip");
+var details = d3.select("#details")
+  .style("opacity",1);
+var topic = d3.select("#topic");
+var topicDesc = d3.select("#topic-description");
+
+// Draw SVG and G
+var svg = d3.select("#dendrogram")
+    .append("svg")
+    .attr("width",width)
+    .attr("height",height)
+    .attr("id","#dendrogram-svg");
+
+// Draw background circles
+var gBackground = svg.append("g")
+    .attr("id","background ")
+    .attr("transform", "translate(" + radius + "," + radius + ")");
+
+for (i=0; i<depth; i++) {
+    gBackground.append("circle")
+        .attr("class","background-circle")
+        .attr("r", distance*i);
+};
+
+// Get ready to draw the tree!
+var g = svg.append("g")
+    .attr("id","tree")
+    .attr("transform", "translate(" + radius + "," + radius + ")");
+    
+
+
+// Connector Function
+function connector(d) {
+    return "M" + project(d.x, d.y)
+        + "C" + project(d.x, (d.y + d.parent.y) / 2)
+        + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
+        + " " + project(d.parent.x, d.parent.y)
+};
+
+
+
+var treeMap = d3.tree()
+    .size([width/2,height/2]), // this is how much of the circumference the outer ring of nodes takes up
+    root;
+var nodeSvg, linkSvg, nodeEnter, linkEnter;
+
+    d3.json("data/vcContent.json",function(error,treeData){
+        if(error) throw error;
+
+    
+        root = d3.hierarchy(treeData,function(d){
+            return d.children;
+        });
+
+        console.log(treeData, "turns into ", root);
+
+        root.each(function (d) {
+            d.name = d.data.name; //transferring name to a name variable
+            d.id = i; //Assigning numerical Ids
+            i += i;
+        });
+
+        root.x0 = height / 2;
+        root.y0 = 0;
+
+        function collapse(d) {
+            if (d.children) {
+                d._children = d.children;
+                d._children.forEach(collapse);
+                d.children = null;
+            }
+        };
+        
+        //root.children.forEach(collapse);
+        update(root);
+    });
+
+
+function update(source) {
+    //root = treeMap(root);
+    nodes = treeMap(root).descendants();
+    //console.log(nodes);
+    //links = root.descendants().slice(1);
+    links = nodes.slice(1);
+    //console.log(links);
+    var nodeUpdate;
+    var nodeExit;
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * distance; });
+
+    nodeSvg = g.selectAll(".node")
+        .data(nodes,function(d) { return d.id || (d.id = ++i); });
+       
+
+    //nodeSvg.exit().remove();
+
+    var nodeEnter = nodeSvg.enter()
+        .append("g")
+        //.attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; })
+        //.attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+        .on("click",click)
+        .on("mouseover", function(d) { return "minu"; });
+
+    nodeEnter.append("circle")
+        .attr("r", function(d){ return maxRad/d.data.depth })
+        .style("fill", color)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
+
+
+    nodeEnter.append("text")
+        .attr("dy", ".15em")
+        .attr("x", function(d) { return d.x < 180 === !d.children ? 10 : -10; })
+        .attr("class","node-label")
+        .style("text-anchor", function(d) { return d.x < 180 === !d.children ? "start" : "end"; })
+        .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; }) // adjust this as needed for text rotation
+        .text(function(d) {  return d.data.name; });
+
+    // Transition nodes to their new position.
+    var nodeUpdate = nodeSvg.merge(nodeEnter).transition()
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; });
+
+
+    nodeSvg.select("circle")
+        .style("fill", color);
+
+
+    nodeUpdate.select("text")
+        .style("fill-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    var nodeExit = nodeSvg.exit().transition()
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; }) //for the animation to either go off there itself or come to centre
+        .remove();
+
+    nodeExit.select("circle")
+        .attr("r", 0);
+
+    nodeExit.select("text")
+        .style("fill-opacity", 0);
+
+    nodes.forEach(function(d) {
+        d.x0 = d.x;
+        d.y0 = d.y;
+    });
+
+
+    linkSvg = g.selectAll(".link")
+        .data(links, function(link) { var id = link.id + '->' + link.parent.id; return id; });
+    
+    // Transition links to their new position.
+    linkSvg.transition()
+        .duration(duration);
+        // .attr('d', connector);
+
+    // Enter any new links at the parent's previous position.
+    linkEnter = linkSvg.enter().insert('path', 'g')
+        .attr("class", "link")
+        .attr("d", function(d) {
+            return "M" + project(d.x, d.y)
+                    + "C" + project(d.x, (d.y + d.parent.y) / 2)
+                    + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
+                    + " " + project(d.parent.x, d.parent.y);
+        });
+        /*
+        function (d) {
+            var o = {x: source.x0, y: source.y0, parent: {x: source.x0, y: source.y0}};
+            return connector(o);
+        });*/
+
+    // Transition links to their new position.
+    linkSvg.merge(linkEnter).transition()
+        .duration(duration)
+        .attr("d", connector);
+
+
+    // Transition exiting nodes to the parent's new position.
+    linkSvg.exit().transition()
+        .duration(duration)
+        .attr("d", /*function (d) {
+            var o = {x: source.x, y: source.y, parent: {x: source.x, y: source.y}};
+            return connector(o);
+        })*/function(d) {
+                    return "M" + project(d.x, d.y)
+                            + "C" + project(d.x, (d.y + d.parent.y) / 2)
+                            + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
+                            + " " + project(d.parent.x, d.parent.y);
+                })
+        .remove();
+};
+
+function click(d) {
+    topic.text(d.data.name);
+    topicDesc.text(d.data.description);
+
+  if (d.children) {
+    d._children = d.children;
+    d.children = null;
+  } else {
+    d.children = d._children;
+    d._children = null;
+  }
+  update(d);
+}
+
+function color(d) {
+  return d._children ? colors.collapsed // collapsed package
+      : d.children ? colors.expanded // expanded package
+      : colors.leaf; // leaf node
+}
+
+
+function flatten (root) {
+  // hierarchical data to flat data for force layout
+  var nodes = [];
+  function recurse(node) {
+    if (node.children) node.children.forEach(recurse);
+    if (!node.id) node.id = ++i;
+    else ++i;
+    nodes.push(node);
+  }
+  recurse(root);
+  return nodes;
+}
+
+function project(x, y) {
+  var angle = (x - 90) / 180 * Math.PI, radius = y;
+  return [radius * Math.cos(angle), radius * Math.sin(angle)];
+}
+
+// hover
+var mouseover = function(d){
+    tooltip.text(d.name)
+        .style("opacity",1);
+    d3.select(this)
+      .style("stroke", "black")
+      .style("fill", colors.highlight)
+      .style("opacity", 1);
+  };
+
+// mouse move
+var mousemove = function(d) {
+    tooltip
+        .style("left", (d3.event.pageX + 5) + "px")
+        .style("top", (d3.event.pageY + 20) + "px");
+};
+  
+// mouse leave
+var mouseleave = function(d) {
+    tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .attr("class", "visited")
+  };
+
+
+
+// Buttons
+var nodeLabels = g.selectAll("node-label");
+var checkBox = document.getElementById("node-label-toggle")
+    .on("click", nodeLabelToggle);
+
+var nodeLabelToggle = function() {
+    // Get the checkbox
+
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        console.log("hey");
+        nodeLabels.style("opacity","1");
+    } else {
+        console.log("nope");
+        nodeLabels.style("opacity","0");
+    }
+  }
