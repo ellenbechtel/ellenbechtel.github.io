db72f2b87bd153e801ec0fed1e63beedad0729d8
ellen <ellen.bechtel@gmail.com>
2021-06-14 16:05:57 -0400

    upload gsap

---
+++ projects/recreation-thursday/hlito/gsap.js
@@ -0,0 +1,58 @@
+// declare colors
+let background = "#dad4c9"
+let yellow = "#c7b36c"
+let blue = "#175485"
+let green = "#7b9d54"
+let red = "#c24045"
+let duration = 1000
+let time = 0.7;
+let delay = -0.3
+let stagger = 0.2;
+
+// create d3 function to invert colors
+const invertColors = () => {
+
+    d3.select("#background")
+        .transition()
+        .duration(duration)
+        .style("fill", background)
+    
+    d3.selectAll(".yellow-blue")
+        .transition()
+        .duration(duration)
+        .style("stroke", yellow)
+    
+    d3.selectAll(".blue-orange")
+        .transition()
+        .duration(duration)
+        .style("stroke", blue)
+    
+    d3.selectAll(".green-purp")
+        .transition()
+        .duration(duration)
+        .style("stroke", green)
+    
+    d3.selectAll(".red-teal")
+        .transition()
+        .duration(duration)
+        .style("stroke", red)
+    
+    d3.selectAll(".black-white")
+        .transition()
+        .duration(duration)
+        .style("stroke", "black")
+}
+
+
+// Create Timeline
+const tl = gsap.timeline();
+
+// Add animations to timeline
+
+tl.from("#big-curve", { x: -100, y: 400, rotation: 90, duration: time, delay: delay, stagger: stagger })
+tl.from("#small-curve", { x: 500, y: 90, rotation: 400, duration: time, delay: 0, stagger: stagger })
+tl.from("#straight-line", { x: 500, y: 180, rotation: 27, duration: time, delay: 0, stagger: stagger })
+// tl.from(".black-white", { x: 100, rotation: 450, duration: 2 })
+tl.call(invertColors)
+// tl.to(".line, .circ", {x: 600, delay: 1, duration: 2}) // disappear offscreen
+
