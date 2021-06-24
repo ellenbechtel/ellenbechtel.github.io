0e193eeae3a41401757f93b4f48450cb4631f9ad
ellen <ellen.bechtel@gmail.com>
2021-06-24 14:24:39 -0400

    stuck on drawing a sine line

--- projects/recreation-thursday/twilight-wave/scripts/twilightWave.js
+++ projects/recreation-thursday/twilight-wave/scripts/twilightWave.js
@@ -11,3 +11,47 @@ let time = 0.7;
 let delay = -0.3
 let stagger = 0.175;
 
+// declare svg properties
+let svg = d3.select("#svg"),
+    width = 500,
+    height = 500;
+
+let gLine = d3.select("#gLine");
+
+svg.style({
+    'width': width,
+    'height': height
+});
+
+let sine = d3.range(0, 10).map(function(k) {
+  let value = [0.5 * k * Math.PI, Math.sin(0.5 * k * Math.PI)];
+  return value;
+});
+
+// console.log("sine",sine, d3.extent(sine, function(d){ return d[0] }))
+
+let xScale = d3.scaleLinear()
+    .range([0, width])
+    .domain([d3.extent(sine, function (d) {
+        console.log(d[0])
+        return d[0];
+    }
+    )]);
+
+console.log("test scale");
+let yScale = d3.scaleLinear()
+    .range([height, 0])
+    .domain([-1, 1]);
+
+let line = d3.line()
+    .x(function (d, i) { console.log(xScale(d)); return xScale(d[0]); }) // set the x values for the line generator
+    .y(function(d) { return yScale(d[1]); }) // set the y values for the line generator 
+    // .curve(d3.curveMonotoneX) // apply smoothing to the line
+
+
+ gLine.append("path")
+    .datum(sine) // 10. Binds data to the line 
+    .attr("class", "line") // Assign a class for styling 
+    .attr("d", line); // 11. Calls the line generator 
+
+
