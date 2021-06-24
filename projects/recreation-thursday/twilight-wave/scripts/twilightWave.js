7c31dd2fed474b1f2b75286382a8740c3da5877f
ellen <ellen.bechtel@gmail.com>
2021-06-24 13:36:37 -0400

    add twilightWave structure

---
+++ projects/recreation-thursday/twilight-wave/scripts/twilightWave.js
@@ -0,0 +1,13 @@
+// declare colors
+let background = "#dad4c9"
+let yellow = "#c7b36c"
+let blue = "#175485"
+let green = "#7b9d54"
+let red = "#c24045"
+
+// declare animation constants
+let duration = 1000
+let time = 0.7;
+let delay = -0.3
+let stagger = 0.175;
+
